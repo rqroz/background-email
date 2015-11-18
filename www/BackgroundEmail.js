8208e1e1e27602ae753cc99b5d153b331ba26f58
rqroz <ryu-shi@hotmail.com>
2015-11-18 14:56:17 -0600

    Update BackgroundEmail.js

--- www/BackgroundEmail.js
+++ www/BackgroundEmail.js
@@ -1,22 +1,22 @@
 cordova.define("com-spmt-rqueiroz-BackgroundEmail.BackgroundEmail", function(require, exports, module) {
-               module.exports = {
-                    send: function(emailObj, successCallback, failCallback) {
-                        cordova.exec(successCallback,
-                                     failCallback,
-                                     "BackgroundEmail",
-                                     "send",
-                                     [
-                                      emailObj.from,
-                                      emailObj.to,
-                                      emailObj.relayHost,
-                                      emailObj.requiresAuth,
-                                      emailObj.login,
-                                      emailObj.password,
-                                      emailObj.subject,
-                                      emailObj.wantsSecure,
-                                      emailObj.body
-                                      ]
-                        );
-                    }
-                };
+  module.exports = {
+    send: function(emailObj, successCallback, failCallback) {
+      cordova.exec(
+        successCallback,
+        failCallback,
+        "BackgroundEmail",
+        "sendEmail",
+        [
+          emailObj.from,
+          emailObj.to,
+          emailObj.subject,
+          emailObj.body,
+          emailObj.login,
+          emailObj.password,
+          emailObj.relayHost,
+          emailObj.port
+        ]
+      );
+    }
+  };
 });
