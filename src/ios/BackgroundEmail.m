9fef3e9f7b89746c69812eb83e0f36d693fe7bfa
rqroz <ryu-shi@hotmail.com>
2015-12-08 09:26:36 -0600

    Update BackgroundEmail.m

--- src/ios/BackgroundEmail.m
+++ src/ios/BackgroundEmail.m
@@ -1,4 +1,4 @@
-/** 
+/**
  The MIT License (MIT)
  
  Copyright (c) 2015 Rodolfo Natan Silva Queiroz.
@@ -32,58 +32,61 @@
 
 
 @implementation BackgroundEmail: CDVPlugin
-    @synthesize callbackID;
-    -(void)pluginInitialize{
+@synthesize callbackID;
+@synthesize email = _email;
 
-    }
+-(void)pluginInitialize{
+    
+}
 
-    - (void)sendEmail:(CDVInvokedUrlCommand*)command
-    {
-        [self.commandDelegate runInBackground:^{
-            SKPSMTPMessage *email = [[SKPSMTPMessage alloc] init];
-            @try {
-                self.callbackID = command.callbackId;
-                CDVInvokedUrlCommand *arguments = [command argumentAtIndex:0];
-                email.fromEmail = [arguments valueForKey:@"from"];
-                email.toEmail = [arguments valueForKey:@"to"];
-                email.ccEmail = [arguments valueForKey:@"cc"];
-                email.bccEmail = [arguments valueForKey:@"bcc"];
-                email.subject = [arguments valueForKey:@"subject"];
-                email.login = [arguments valueForKey:@"login"];
-                email.pass = [arguments valueForKey:@"password"];
-                email.relayHost = [arguments valueForKey:@"relayHost"];
-                email.requiresAuth = [[arguments valueForKey:@"auth"] boolValue];
-                email.wantsSecure = [[arguments valueForKey:@"security"] boolValue];
-                email.validateSSLChain = [[arguments valueForKey:@"SSL"] boolValue];
-                email.delegate = self;
-                
-                NSArray *ports = [[NSMutableArray alloc] initWithArray:[arguments valueForKey:@"ports"] copyItems:YES];
-                if (!(!ports || !ports.count)) {
-                    email.relayPorts = [[NSArray alloc] initWithArray:ports];
-                }
-                
-                NSString *bodyMessage = [arguments valueForKey:@"body"];
-                NSDictionary *actualMessage = [NSDictionary dictionaryWithObjectsAndKeys:@"text/plain", kSKPSMTPPartContentTypeKey, bodyMessage, kSKPSMTPPartMessageKey, @"8bit", kSKPSMTPPartContentTransferEncodingKey, nil];
-                email.parts = [NSArray arrayWithObjects:actualMessage, nil];
-                
-                [email send];
+- (void)sendEmail:(CDVInvokedUrlCommand*)command
+{
+    
+    [self.commandDelegate runInBackground:^{
+        _email = [[SKPSMTPMessage alloc] init];
+        @try {
+            self.callbackID = command.callbackId;
+            CDVInvokedUrlCommand *arguments = [command argumentAtIndex:0];
+            _email.fromEmail = [arguments valueForKey:@"from"];
+            _email.toEmail = [arguments valueForKey:@"to"];
+            _email.ccEmail = [arguments valueForKey:@"cc"];
+            _email.bccEmail = [arguments valueForKey:@"bcc"];
+            _email.subject = [arguments valueForKey:@"subject"];
+            _email.login = [arguments valueForKey:@"login"];
+            _email.pass = [arguments valueForKey:@"password"];
+            _email.relayHost = [arguments valueForKey:@"relayHost"];
+            _email.requiresAuth = [[arguments valueForKey:@"auth"] boolValue];
+            _email.wantsSecure = [[arguments valueForKey:@"security"] boolValue];
+            _email.validateSSLChain = [[arguments valueForKey:@"SSL"] boolValue];
+            _email.delegate = self;
+            
+            NSArray *ports = [[NSMutableArray alloc] initWithArray:[arguments valueForKey:@"ports"] copyItems:YES];
+            if (!(!ports || !ports.count)) {
+                _email.relayPorts = [[NSArray alloc] initWithArray:ports];
             }
-            @catch (NSException *exception) {
-                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[exception description]];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
-            }
-        }];
-    }
+            
+            NSString *bodyMessage = [arguments valueForKey:@"body"];
+            NSDictionary *actualMessage = [NSDictionary dictionaryWithObjectsAndKeys:@"text/plain", kSKPSMTPPartContentTypeKey, bodyMessage, kSKPSMTPPartMessageKey, @"8bit", kSKPSMTPPartContentTransferEncodingKey, nil];
+            _email.parts = [NSArray arrayWithObjects:actualMessage, nil];
+            
+            [_email send];
+        }
+        @catch (NSException *exception) {
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[exception description]];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
+        }
+    }];
+}
 
-    -(void)messageSent:(SKPSMTPMessage *)message{
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Message sent!"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
-    }
+-(void)messageSent:(SKPSMTPMessage *)message{
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Message sent!"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
+}
 
-    -(void)messageFailed:(SKPSMTPMessage *)message error:(NSError *)error{
-        NSString *errorMsg = [NSString stringWithFormat:@"%@\n%@", [error localizedDescription], [error localizedRecoverySuggestion]];
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMsg];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
-    }
+-(void)messageFailed:(SKPSMTPMessage *)message error:(NSError *)error{
+    NSString *errorMsg = [NSString stringWithFormat:@"%@\n%@", [error localizedDescription], [error localizedRecoverySuggestion]];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMsg];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
+}
 
 @end
